237d46827da59f7fbaf4e1230f213040539ec64d
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-01-23 18:19:21 -0300

    Adicionado javascript ao projeto e criado o botao de voltar ao topo

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// FUNCAO PARA QUE A BARRA DE ROLAGEM VOLTE PARA A POSICAO 0 NO EIXO Y.
+
+function scrollBar(){
+    window.scrollTo({
+        top: 0,
+        behavior:'smooth',
+    });
+}
+
+// FUNCAO QUE PARA FAZER COM QUE O BOTAO SUMA QUANDO A POSICAO Y DO SCROLL ESTIVER EM 0
+
+function controlarBotao(){
+    if (window.scrollY === 0){
+        document.querySelector(".btn").style.display = 'none';
+    } else{
+        document.querySelector(".btn").style.display = 'block';
+    }
+}
+
+window.addEventListener('scroll', controlarBotao);
